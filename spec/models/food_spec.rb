e10d67ea713cfc4d06286b2f332855c7ba706fa8
Van Dexter <vanguardstellar@gmail.com>
2022-05-13 11:52:08 -0700

    intial commit

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Create food Validations" do
+    it "must contain a dishname" do
+        food = Food.create genre: "Japanese", category: "breakfast", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwwQumXHqcpaEIk9qYGTZJGc3HmMKnwGBd_uJiq9JsUXlahU7QezZXooEFQ-ZTNLkiD-E&usqp=CAU"
+        expect(food.errors[:dishname]).to_not be_empty
+    end
+    it "must contain a genre" do
+      food = Food.create dishname: "egg sandwich", category: "breakfast", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwwQumXHqcpaEIk9qYGTZJGc3HmMKnwGBd_uJiq9JsUXlahU7QezZXooEFQ-ZTNLkiD-E&usqp=CAU"
+      expect(food.errors[:genre]).to_not be_empty
+    end
+    it "must contain a category" do
+      food = Food.create dishname: "egg sandwich", category: "breakfast", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwwQumXHqcpaEIk9qYGTZJGc3HmMKnwGBd_uJiq9JsUXlahU7QezZXooEFQ-ZTNLkiD-E&usqp=CAU"
+      expect(food.errors[:genre]).to_not be_empty
+    end
+    it "must contain an image" do
+      food = Food.create dishname: "egg sandwich", category: "breakfast"
+      expect(food.errors[:image]).to_not be_empty
+    end
+  end
 end
--- spec/requests/foods_spec.rb
+++ spec/requests/foods_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe "Foods", type: :request do
     expect(food.length).to eq(1)
     end
   end
-describe "Post /create" do
+  describe "Post /create" do
   it 'can create a food' do
     food_params = {
       food: {
@@ -35,16 +35,16 @@ describe "Post /create" do
     end
   end
 
-describe "PATCH / update" do
-  it 'can update an existing food' do
-    food_params = {
-      food: {
-          dishname: 'Spaghetti',
-          genre: 'Italian',
-          category: 'Entree',
-          image: 'https://cdn.pixabay.com/photo/2020/01/07/05/52/spaghetti-4746831_1280.jpg'
+  describe "PATCH / update" do
+    it 'can update an existing food' do
+      food_params = {
+        food: {
+            dishname: 'Spaghetti',
+            genre: 'Italian',
+            category: 'Entree',
+            image: 'https://cdn.pixabay.com/photo/2020/01/07/05/52/spaghetti-4746831_1280.jpg'
+        }
       }
-    }
     post '/foods', params: food_params
     food = Food.first
 
@@ -62,4 +62,86 @@ describe "PATCH / update" do
         expect(updated_food.category).to eq('Entree & Dinner')
     end
   end
-end
+  describe 'Food create request validations' do
+    it "doesn't create a food without a dishname" do
+      food_params = {
+        food: {
+            genre: 'Italian',
+            category: 'Entree',
+            image: 'https://cdn.pixabay.com/photo/2020/01/07/05/52/spaghetti-4746831_1280.jpg'
+        }
+      }
+      post '/foods', params: food_params
+      expect(response.status).to eq 422
+      expect(response).to have_http_status(422)
+
+        json = JSON.parse(response.body)
+      expect(json['dishname']).to include "can't be blank"
+
+    end
+      it 'has to create a dishname with a minimum of three characters' do
+        food_params = {
+          food: {
+              dishname: 'd',
+              genre: 'Italian',
+              category: 'Entree',
+              image: 'https://cdn.pixabay.com/photo/2020/01/07/05/52/spaghetti-4746831_1280.jpg'
+          }
+        }      
+      post '/foods', params: food_params
+      json = JSON.parse(response.body)    
+          expect(response).to have_http_status(422)
+          expect(json['dishname'].length).to be <= 3
+          # expect(json['dishname']).to include "can't be blank"
+      end
+    
+      it "doesn't create a food without a genre" do
+        food_params = {
+          food: {
+              dishname: 'Spaghetti',
+              category: 'Entree',
+              image: 'https://cdn.pixabay.com/photo/2020/01/07/05/52/spaghetti-4746831_1280.jpg'
+          }
+        }
+        post '/foods', params: food_params
+        expect(response.status).to eq 422
+        expect(response).to have_http_status(422)
+
+          json = JSON.parse(response.body)
+        expect(json['genre']).to include "can't be blank"
+    end
+      it "doesn't create a food without a category" do
+        food_params = {
+          food: {
+              dishname: 'Spaghetti',
+                genre: 'Italian',
+              image: 'https://cdn.pixabay.com/photo/2020/01/07/05/52/spaghetti-4746831_1280.jpg'
+          }
+        }
+        post '/foods', params: food_params
+        expect(response.status).to eq 422
+        expect(response).to have_http_status(422)
+
+          json = JSON.parse(response.body)
+        expect(json['category']).to include "can't be blank"
+
+    end
+
+    it "doesn't create a food without an image" do
+      food_params = {
+        food: {
+            dishname: 'Spaghetti',
+              genre: 'Italian',
+            category: 'Entree'
+        }
+      }
+      post '/foods', params: food_params
+      expect(response.status).to eq 422
+      expect(response).to have_http_status(422)
+
+        json = JSON.parse(response.body)
+      expect(json['image']).to include "can't be blank"
+    end
+
+  end
+end  
